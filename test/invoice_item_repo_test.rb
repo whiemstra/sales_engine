ed6e5ff02537b006364c8750373bb5988b5d6089
brettgrigsby <gbrett@gmail.com>
2015-04-22 08:56:27 -0600

    small things

--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -4,41 +4,41 @@ require 'minitest/pride'
 require_relative '../lib/invoice_item_repo'
 require_relative '../lib/sales_engine'
 
-class CustomerRepoTest < MiniTest::Test
+class InvoiceItemRepo < MiniTest::Test
   def test_points_back_to_its_parent
     se = SalesEngine.new
-    assert se, se.customer_repo.engine
+    assert se, se.invoice_item_repo.engine
   end
 
   def test_can_populate_from_csv_file
     se = SalesEngine.new
     se.startup
-    assert se.customer_repo.customers
+    assert se.invoice_item_repo.invoice_items
   end
 
   def test_all_returns_an_array_of_invoice_items
     se = SalesEngine.new
     se.startup
-    assert_equal Array, se.customer_repo.invoice_items.class
+    assert_equal Array, se.invoice_item_repo.invoice_items.class
   end
 
   def test_random_returns_one_random_invoice_item_obj
     se = SalesEngine.new
     se.startup
-    assert_equal Customer, se.customer_repo.random.class
+    assert_equal Customer, se.invoice_item_repo.random.class
   end
 
   def test_find_an_invoice_item_by_id
     se = SalesEngine.new
     se.startup
-    assert_equal "Katrina", se.customer_repo.find_by_id(13).first_name
+    assert_equal 1917, se.invoice_item_repo.find_by_id(19).item_id
   end
 
   def test_find_all_invoice_items_by_first_name
     se = SalesEngine.new
     se.startup
-    assert_equal 2, se.customer_repo.find_all_by_first_name("Kim").size
-    assert_equal 1, se.customer_repo.find_all_by_first_name("Ruby").size
+    assert_equal 2, se.invoice_item_repo.find_all_by_first_name("Kim").size
+    assert_equal 1, se.invoice_item_repo.find_all_by_first_name("Ruby").size
   end
 
 end
