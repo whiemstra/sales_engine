1a50b41871d802d4a915e8d5767a6bf927ba6c35
brettgrigsby <gbrett@gmail.com>
2015-04-22 10:14:47 -0600

    working invoice_item_repo_test

--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 require_relative '../lib/invoice_item_repo'
 require_relative '../lib/sales_engine'
 
-class InvoiceItemRepo < MiniTest::Test
+class InvoiceItemRepoTest < MiniTest::Test
   def test_points_back_to_its_parent
     se = SalesEngine.new
     assert se, se.invoice_item_repo.engine
@@ -25,7 +25,7 @@ class InvoiceItemRepo < MiniTest::Test
   def test_random_returns_one_random_invoice_item_obj
     se = SalesEngine.new
     se.startup
-    assert_equal Customer, se.invoice_item_repo.random.class
+    assert_equal InvoiceItem, se.invoice_item_repo.random.class
   end
 
   def test_find_an_invoice_item_by_id
@@ -34,11 +34,10 @@ class InvoiceItemRepo < MiniTest::Test
     assert_equal 1917, se.invoice_item_repo.find_by_id(19).item_id
   end
 
-  def test_find_all_invoice_items_by_first_name
+  def test_find_all_invoice_items_by_quantity
     se = SalesEngine.new
     se.startup
-    assert_equal 2, se.invoice_item_repo.find_all_by_first_name("Kim").size
-    assert_equal 1, se.invoice_item_repo.find_all_by_first_name("Ruby").size
+    assert_equal 2231, se.invoice_item_repo.find_all_by_quantity(4).size
   end
 
 end
