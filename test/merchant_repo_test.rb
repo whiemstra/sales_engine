ca60ab70e8645585d3c77fd132dc7f084856d5b8
Whitney Hiemstra <whitcolorado@gmail.com>
2015-04-21 22:21:37 -0600

    merchant_repo testing

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -16,7 +16,7 @@ class MerchantRepoTest < MiniTest::Test
     assert se.merchant_repo.merchants
   end
 
-  def test_all_returns_an_array_of_merchants
+  def test_all_returns_an_array_of_all_merchants
     se = SalesEngine.new
     se.startup
     assert_equal Array, se.merchant_repo.merchants.class
@@ -34,9 +34,41 @@ class MerchantRepoTest < MiniTest::Test
     assert_equal "Williamson Group", se.merchant_repo.find_by_id(5).name
   end
 
+  def test_find_a_merchant_by_name
+    se = SalesEngine.new
+    se.startup
+    assert_equal "Crona LLC", se.merchant_repo.find_by_name("Crona LLC").name
+  end
+
+  def test_it_can_find_by_created_at
+    se = SalesEngine.new
+    se.startup
+    assert_equal "2012-03-27 14:53:59 UTC", se.merchant_repo.find_by_created_at("2012-03-27 14:53:59 UTC").created_at
+  end
+
+  def test_it_can_find_by_updated_at
+    se = SalesEngine.new
+    se.startup
+    assert_equal "2012-03-27 14:54:09 UTC", se.merchant_repo.find_by_updated_at("2012-03-27 14:54:09 UTC").updated_at
+  end
+
   def test_find_all_merchants_by_name
     se = SalesEngine.new
     se.startup
     assert_equal 2, se.merchant_repo.find_all_by_name("Williamson Group").size
+  end
+
+  def test_it_can_find_all_by_created_at
+    se = SalesEngine.new
+    se.startup
+    result = se.merchant_repo.find_all_by_created_at("2012-03-27 14:53:59 UTC")
+    assert_equal 9, result.count
+  end
+
+  def test_it_can_find_all_by_updated_at
+    se = SalesEngine.new
+    se.startup
+    result = se.merchant_repo.find_all_by_updated_at("2012-03-27 14:54:09 UTC")
+    assert_equal 4, result.count
   end
 end
