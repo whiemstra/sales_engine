2d5d386d31ad1af145aa6bc46fc9ed477881b88e
brettgrigsby <gbrett@gmail.com>
2015-04-28 15:30:26 -0600

    the little things

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,34 +1,34 @@
 require 'csv'
 
 class CSVParser
-  def self.parse_merchants(filepath)
-    parse(filepath, 'merchants.csv')
+  def self.parse_merchants(dirname)
+    parse(dirname, 'merchants.csv')
   end
 
-  def self.parse_customers(filepath)
-    parse(filepath, 'customers.csv')
+  def self.parse_customers(dirname)
+    parse(dirname, 'customers.csv')
   end
 
-  def self.parse_items(filepath)
-    parse(filepath, 'items.csv')
+  def self.parse_items(dirname)
+    parse(dirname, 'items.csv')
   end
 
-  def self.parse_invoice_items(filepath)
-    parse(filepath, 'invoice_items.csv')
+  def self.parse_invoice_items(dirname)
+    parse(dirname, 'invoice_items.csv')
   end
 
-  def self.parse_invoices(filepath)
-    parse(filepath, 'invoices.csv')
+  def self.parse_invoices(dirname)
+    parse(dirname, 'invoices.csv')
   end
 
-  def self.parse_transactions(filepath)
-    parse(filepath, 'transactions.csv')
+  def self.parse_transactions(dirname)
+    parse(dirname, 'transactions.csv')
   end
 
   private
 
   def self.parse(dirname, basename)
-    CSV.open  File.join(dirname, basename), 
+    CSV.open  File.join(dirname, basename),
               headers: true,
               header_converters: :symbol
   end
